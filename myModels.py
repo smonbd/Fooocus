270bd40e563025b2809c98cf3c127785374bb631
smonbd <155661018+smonbd@users.noreply.github.com>
2025-04-09 13:16:01 +0600

    Update myModels.py

--- myModels.py
+++ myModels.py
@@ -13,10 +13,10 @@ headers = {
 
 # List of models to download
 files = [
-    #{
-        #"url": "https://civitai.com/api/download/models/798204?type=Model&format=SafeTensor&size=full&fp=fp16",
-        #"path": "models/checkpoints/RealVisXL_V5.safetensors"
-    #},
+    {
+        "url": "https://civitai.com/api/download/models/1422871?type=Model&format=SafeTensor&size=pruned&fp=fp16",
+        "path": "models/checkpoints/realiSim_By_Yogi.safetensors"
+    },
     {
         "url": "https://civitai.com/api/download/models/131991?type=Model&format=SafeTensor",
         "path": "models/loras/jugganath_cinematic.safetensors"
