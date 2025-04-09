b02ee3c42886b1bdc9187ad5c4b88e6ff75eb8da
smonbd <155661018+smonbd@users.noreply.github.com>
2025-04-10 00:32:56 +0600

    Update myModels.py

--- myModels.py
+++ myModels.py
@@ -48,6 +48,10 @@ files = [
     {
         "url": "https://civitai.com/api/download/models/135867?type=Model&format=SafeTensor",
         "path": "models/loras/Details_Twicker.safetensors"
+    },
+    {
+        "url": "https://civitai.com/api/download/models/1242203?type=Model&format=SafeTensor",
+        "path": "models/loras/Dramadic_Light.safetensors"
     }
 ]
 
