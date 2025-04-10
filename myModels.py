078b5204c7d8cfbb80092c5d640e335f242604a6
smonbd <155661018+smonbd@users.noreply.github.com>
2025-04-10 12:22:46 +0600

    Update myModels.py

--- myModels.py
+++ myModels.py
@@ -44,6 +44,10 @@ files = [
     {
         "url": "https://civitai.com/api/download/models/1242203?type=Model&format=SafeTensor",
         "path": "models/loras/Dramadic_Light.safetensors"
+    },
+    {
+        "url": "https://civitai.com/api/download/models/471781?type=Model&format=SafeTensor",
+        "path": "models/loras/feet.safetensors"
     }
 ]
 
